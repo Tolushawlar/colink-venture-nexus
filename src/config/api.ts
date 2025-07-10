c64515ebe25eef16c65485e8df1cdd33f964e760
Tolushawlar <Tolushaw.it@gmail.com>
2025-07-10 15:46:12 +0100

    api route changed

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,7 @@
 // API Configuration
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://colinksbackend.onrender.com'
+
+// 'http://localhost:3000/api';
 
 export const API_CONFIG = {
   API_BASE_URL
